d8212b598014e332078c7b8461847594c5f5c9ee
Breekacho0 <Breekacho0allesi95@gmail.com>
2018-05-11 14:44:47 +0300

    recent changes

--- bot.js
+++ bot.js
@@ -42,35 +42,30 @@ bot.on('message', (msg) => {
       return 1;
     }
   }
-  if (StickerWL.indexOf(data.from.username) == -1 && data.hasOwnProperty("sticker")) {
+  if (data.hasOwnProperty("sticker")) {
     console.log(data.sticker);
-    opts = {
-      parse_mode: 'Markdown'
-    };
-    if (data.sticker.set_name.indexOf('katyaserebro') != -1 ||
-      data.sticker.set_name.indexOf('katya_kishuk') != -1 ||
-      data.sticker.set_name.indexOf('KatyaKishchuk') != -1) {
-      text_message = '[' + user.first_name + '](tg://user?id=' + user.id + ') твой стикер удален. Кищук вип.';
-      bot.deleteMessage(chat_id, message_id);
-      bot.sendMessage(chat_id, text_message, opts);
-      console.log("Sticker from " + user.id + " deleted");
-      return 1;
-    }
-    /*if (data.sticker.set_name.indexOf('PopugiandAgurtsy') != -1 ||
-      data.sticker.set_name.indexOf('blyadskiepapugi') != -1 ||
-      data.sticker.set_name.indexOf('BirbsForMemes') != -1) {
-      text_message = '[' + user.first_name + '](tg://user?id=' + user.id + ') твой стикер удален. удален. Это вип попуги.';
-      bot.deleteMessage(chat_id, message_id);
-      bot.sendMessage(chat_id, text_message, opts);
-      console.log("Sticker from " + user.id + " deleted");
-      return 1;
-    }*/
-    if (data.sticker.set_name.indexOf('SpurdoFaces') != -1) {
-      text_message = '[' + user.first_name + '](tg://user?id=' + user.id + ') твой стикер удален. Вип стикер.';
-      bot.deleteMessage(chat_id, message_id);
-      bot.sendMessage(chat_id, text_message, opts);
-      console.log("Sticker from " + user.id + " deleted");
-      return 1;
+    if (StickerWL.indexOf(data.from.username) == -1) {
+      opts = {
+        parse_mode: 'Markdown'
+      };
+      if (data.sticker.hasOwnProperty("set_name")) {
+        if (data.sticker.set_name.indexOf('katyaserebro') != -1 ||
+          data.sticker.set_name.indexOf('katya_kishuk') != -1 ||
+          data.sticker.set_name.indexOf('KatyaKishchuk') != -1) {
+          text_message = '[' + user.first_name + '](tg://user?id=' + user.id + ') твой стикер удален. Кищук вип.';
+          bot.deleteMessage(chat_id, message_id);
+          bot.sendMessage(chat_id, text_message, opts);
+          console.log("Sticker from " + user.id + " deleted");
+          return 1;
+        }
+        if (data.sticker.set_name.indexOf('SpurdoFaces') != -1) {
+          text_message = '[' + user.first_name + '](tg://user?id=' + user.id + ') твой стикер удален. Вип стикер.';
+          bot.deleteMessage(chat_id, message_id);
+          bot.sendMessage(chat_id, text_message, opts);
+          console.log("Sticker from " + user.id + " deleted");
+          return 1;
+        }
+      }
     }
   }
   //Check if message contains Game attribute
